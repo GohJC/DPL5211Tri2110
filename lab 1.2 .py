7d7cd1c2f0b23ee779dd04d69fa4cb4d380de874
GohJC <90165868+GohJC@users.noreply.github.com>
2021-09-06 10:39:32 +0800

    Add files via upload

---
+++ lab 1.2 .py
@@ -0,0 +1,10 @@
+# Lab 1.2 #
+
+number_1 = int(input("Please enter number 1 : "))
+number_2 = int(input("Please enter number 2 : "))
+
+total = number_1+number_2
+
+# print("The total number is ",total) #
+
+print("The sum of ",number_1,"and",number_2,"is",total)
