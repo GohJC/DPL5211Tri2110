18d28ca4f1c7aeb4defcf7a87b76f52f510fafd3
GohJC <90165868+GohJC@users.noreply.github.com>
2021-09-13 10:35:50 +0800

    Create Lab 2 Q1.py

---
+++ Lab 2 Q1.py
@@ -0,0 +1,6 @@
+#student ID: 1201201564
+#Student Name: Goh Jia Chen
+
+kilo = float(input("Please enter the value of kilogram : "))
+gram = kilo * 1000
+print("For ", kilo, "kilo gram is equilavent to ",gram," gram")
